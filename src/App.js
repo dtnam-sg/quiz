67b625d24b2545d65bc3d6544da133ad37558445
Nam Doan <dtnam.sg@gmail.com>
2020-02-29 14:41:34 +0700

    Initial project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Components/Greet';
+
+function App() {
+    return (
+        <div className="App">
+        <Greet/>
+        </div>
+    );
+
+
+    }
+
+    export default App;
---
+++ src/Components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function Hello(){
+    return(
+        <h1>Hello Dude.</h1>
+    );
+}
+// export const Hello= () => <h1>Hello dude.</h1> Arrow function
+export default Hello;
